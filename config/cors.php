b10d73efe1f0b74684fb99692f0a1806dd2114a1
Naakoi <34052953+Naakoi@users.noreply.github.com>
2026-03-02 11:05:12 +1200

    Add docs folder for GitHub Pages deployment and update API image paths

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://naakoi.github.io',
+        'https://phplaravel-1593166-6235114.cloudwaysapps.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
